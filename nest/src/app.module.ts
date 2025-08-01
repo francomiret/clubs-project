7a8c8067f667aa8661f116bd168aa6d71733a0c5
Franco Miret <francomiret11@gmail.com>
2025-08-01 10:11:45 -0300

    feat: add modules in app module

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { ClubsModule } from './clubs/clubs.module';
 import { CommonModule } from './common/common.module';
+import { ClubsModule } from './clubs/clubs.module';
+import { UsersModule } from './users/users.module';
+import { MembersModule } from './members/members.module';
+import { SponsorsModule } from './sponsors/sponsors.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [PrismaModule, CommonModule, ClubsModule],
+  imports: [
+    PrismaModule,
+    CommonModule,
+    ClubsModule,
+    UsersModule,
+    MembersModule,
+    SponsorsModule,
+    PaymentsModule,
+  ],
   controllers: [],
   providers: [],
 })
